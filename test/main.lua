88f3eb33985358523c3c4fa8925557465bf819b0
Ventro1138 <raamish64@gmail.com>
2015-03-22 01:47:33 -0400

    changed image loaded in main.lua

--- test/main.lua
+++ test/main.lua
@@ -1,14 +1,22 @@
 require("menu") --gets menu
+require ("cityscape") -- gets cityscape background
+require ("road") -- gets road the protagonist travels on
 
+WIDTH = 300
+HEIGHT = 100
+SCALE = 3
+
+bgcolor = {236,273,211,355}
+darkcolor = {12,19,14,255}
 
 
 function love.load()
-   image = love.graphics.newImage("pokeball.png")
+   image = love.graphics.newImage("batshitlogo_000000.png") --cursor maybe
    love.graphics.setNewFont(12)
 end
 
 function love.draw()
    love.graphics.draw(image, imgx, imgy)
-   love.graphics.print("move this shit", 10, 10)
+   love.graphics.print("Circadia", 10, 10)
 end
 
